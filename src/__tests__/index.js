0030ac96299e5f15f1d3232eeeff45c999665c54
madiodio <papigaye1@gmail.com>
2017-12-25 19:40:22 +0000

    Fix snapshot testing and remove "babel-loader"

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -6,7 +6,9 @@ import InlineSVG from '..';
 describe('Component: InlineSVG', () => {
   const Icon = '<svg viewBox="0 0 21 21"><path d="M13 23h7V8L10 .631 0 8v15h7v-7h6v7z"/></svg>';
   it('should match content with specified props', () => {
-    const tree = renderer.create(<InlineSVG icon={Icon} size={20} label="home - house" />).toJSON();
+    const tree = renderer
+      .create(<InlineSVG icon={Icon} size={20} label="home - house" isTest />)
+      .toJSON();
 
     expect(tree).toMatchSnapshot();
   });
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { string, number } from 'prop-types';
+import { bool, string, number } from 'prop-types';
 
 const randomID = `svg-${Math.random()
   .toString(36)
   .substr(2, 5)}`;
 
-const InlineSVG = ({ icon, size, label, ...rest }) => {
-  const a11yTitle = label ? `<title id="${randomID}">Icon - ${label}</title` : '<title></title';
+const InlineSVG = ({ icon, size, label, isTest, ...rest }) => {
+  const titleID = isTest ? 'id0' : randomID;
+  const a11yTitle = label ? `<title id=${titleID}>Icon - ${label}</title` : '<title></title';
   const width = size ? `width="${size}"` : '';
   const height = size ? `height="${size}"` : '';
-  const labelledBy = label ? `aria-labelledby="${randomID}"` : '';
+  const labelledBy = label ? `aria-labelledby="${titleID}"` : '';
 
   const comp = icon.replace(
     /<svg([^>]*)/g,
@@ -22,12 +23,14 @@ const InlineSVG = ({ icon, size, label, ...rest }) => {
 InlineSVG.propTypes = {
   icon: string.isRequired,
   size: number,
-  label: string
+  label: string,
+  isTest: bool
 };
 
 InlineSVG.defaultProps = {
   size: null,
-  label: ''
+  label: '',
+  isTest: false
 };
 
 export default InlineSVG;
